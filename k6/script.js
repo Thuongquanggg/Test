d9eacb8c0b176dd80ee4b65492afe6979c001ab1
Thuongquanggg <huyhoang1999987@gmail.com>
2025-07-29 12:00:34 +0700

    Update script.js

--- k6/script.js
+++ k6/script.js
@@ -3,7 +3,7 @@
 
 import http from 'k6/http';
 import { check, sleep, group } from 'k6';
-const TARGET_URL = 'https://certapple.com';
+const TARGET_URL = 'https://nezushub.vip';
 export const options = {
 scenarios: {
 
